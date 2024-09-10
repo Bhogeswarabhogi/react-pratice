80e1d35adb511f95932d5138ee00cbf86b5feb0a
bhogeswarabhogi <kbhogesh17@gmail.com>
2024-09-10 10:41:48 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import Greeting from "./Greeting";
+import Counter from "./Counter";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Greeting name="Mohan" age={23} />
+        <Greeting name="Rohan" age={25} />
+        <Counter initialCount={10} />
+      </div>
+    );
+  }
+}
+
+export default App;
